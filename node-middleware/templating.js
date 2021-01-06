149ea52e43a4f68e0e9302225d6e8501e7b2c6c8
Shohanur Rahaman <shohan4556@gmail.com>
2021-01-06 23:56:47 +0600

    pug template engine

--- node-middleware/templating.js
+++ node-middleware/templating.js
@@ -2,9 +2,15 @@ const express = require('express')
 const app = express();
 const port = 8080 | process.env.port;
 
-//use templating engine
+//set view engine 
 app.set('view engine', 'pug');
-app.set('view engine')
+
+app.set('views', './views'); //default 
+
+app.get('/', (req, res, next)=>{
+    res.render('index', {title: 'my express app', message:'hello world'})
+    //res.send('hello')
+})
 
 app.listen(port, () => {
     console.log(`server started at : http://127.0.0.1:${port}`);
